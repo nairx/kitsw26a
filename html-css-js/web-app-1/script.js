a8fa0e7bbe294d38ee47623e8c3a26a5b84a9680
nairx <nairxprv@gmail.com>
2026-01-10 12:06:41 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -13,3 +13,23 @@ function dispMessage() {
     lblMessage.innerHTML = "Access Denied";
   }
 }
+
+function showLoginForm(){
+    let str = `
+    <h3>Login Form</h3>
+      <p><label id="lblMessage"></label></p>
+      <p><input type="text" id="txtEmail" /></p>
+      <p><input type="password" id="txtPassword" /></p>
+      <p><button class="login-btn" onclick="dispMessage()">Log In</button></p>
+      <hr>
+      <p><button class="register-btn" onclick="showRegisterForm()">Create Account</button></p>
+    `
+    root.innerHTML = str
+}
+
+function showRegisterForm(){
+    let str =  `
+     <h3>Registration Form</h3>
+    `
+    root.innerHTML = str
+}
